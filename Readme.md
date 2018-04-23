92bdaecda5f4c9c2cbee9ec9fd1de0d9626ed1e5
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ Readme.md
@@ -0,0 +1,9 @@
+# Editing using popup menu
+
+
+<p>This demo shows how to create a link using a DataItemTemplate and populate an ASPxPopupMenu on clicking this link.<br />
+      In this demo the popup menu is used for editing.</p>
+
+<br/>
+
+
---
+++ VB/WebSite/Default.aspx.vb
@@ -0,0 +1,23 @@
+Imports Microsoft.VisualBasic
+Imports System
+Imports System.Data
+Imports System.Configuration
+Imports System.Collections
+Imports System.Web
+Imports System.Web.Security
+Imports System.Web.UI
+Imports System.Web.UI.WebControls
+Imports System.Web.UI.WebControls.WebParts
+Imports System.Web.UI.HtmlControls
+Imports DevExpress.Web.ASPxGridView
+
+Partial Public Class Grid_Editing_CustomUpdate_CustomUpdate
+	Inherits System.Web.UI.Page
+	Protected Sub Page_Load(ByVal sender As Object, ByVal e As EventArgs)
+		'Populate grid with data on the first load
+		If (Not IsPostBack) AndAlso (Not IsCallback) Then
+			Dim provider As New InvoiceItemsProvider()
+			provider.Populate()
+		End If
+	End Sub
+End Class
